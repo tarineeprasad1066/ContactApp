8c69e1fcb4d91688a2c52a2a77229128cb9f6085
tarineeprasad1066 <tarineeprasad1066@gmail.com>
2022-07-18 16:25:07 +0530

    Changed database to postgresql

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
    devise_for :users
    devise_scope :user do  
-   get '/users/sign_out' => 'devise/sessions#destroy'     
+      get '/users/sign_out' => 'devise/sessions#destroy'
+      root :to => 'home#index'     
 end
-   root "home#index"
+   resources :albums
+   resources :users, only: [:show] 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_100859) do
-  create_table "albums", force: :cascade do |t|
-    t.string "name"
-    t.integer "phone"
-    t.string "email"
-    t.string "address"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_albums_on_user_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_112548) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
